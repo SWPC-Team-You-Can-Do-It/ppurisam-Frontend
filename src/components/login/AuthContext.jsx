01662550d49c3b21e49e273a8a7cd5594e1c9405
kittae <jinkitea@naver.com>
2024-11-11 02:13:11 +0900

    feat: 뿌리오 Token 연동

--- src/components/login/AuthContext.jsx
+++ src/components/login/AuthContext.jsx
@@ -6,19 +6,23 @@ export const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
   const [auth, setAuth] = useState({
     access_token: localStorage.getItem("token"),
+    ppurio_token: localStorage.getItem("ppurio_token"),
     isAuthenticated: !!localStorage.getItem("token"),
   });
 
   useEffect(() => {
     const token = localStorage.getItem("token");
+    const ppurioToken = localStorage.getItem("ppurio_token");
     if (token) {
       setAuth({
         access_token: token,
+        ppurio_token: ppurioToken,
         isAuthenticated: true,
       });
     } else {
       setAuth({
         access_token: null,
+        ppurio_token: null,
         isAuthenticated: false,
       });
     }
