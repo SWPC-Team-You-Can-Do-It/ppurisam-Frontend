757f5cd8ff9817f21f1b7806001995d261fcdaec
bkw535 <bkw54570@gmail.com>
2024-11-14 03:23:12 +0900

    feat: 텍스트 생성후 결과값 이미지에 넘겨주기

--- src/pages/SendPage/SendPage.jsx
+++ src/pages/SendPage/SendPage.jsx
@@ -1,5 +1,5 @@
 // SendPage.jsx
-import React from "react";
+import React, { useState } from "react";
 import Header from "@/components/main/Header/Header";
 import Footer from "@/components/main/Footer/Footer";
 import "@/pages/SendPage/SendPage.css";
@@ -7,23 +7,31 @@ import ImageSend from "@/components/send/ImageSend/ImageSend";
 import MessageSend from "@/components/send/MessageSend/MessageSend";
 import SendPageContact from "@/components/send/SendPageContact/SendPageContact";
 
-
 const SendPage = () => {
-    return (
-        <div className="send-container">
-            <Header />
-            <div className="send-content-wrapper">
-                <div className="send-content">
-                    <MessageSend />
-                    <ImageSend />
-                </div>
-            </div>
-            <div className="send-contact-section">
-                <SendPageContact />
-            </div>
-            <Footer />
+  const [generatedPrompt, setGeneratedPrompt] = useState("");
+
+  // MessageSend에서 생성된 메시지를 받는 콜백 함수
+  const handleGeneratedMessage = (message) => {
+    setGeneratedPrompt(message);
+  };
+
+  return (
+    <div className="send-container">
+      <Header />
+      <div className="send-content-wrapper">
+        <div className="send-content">
+          {/* <MessageSend />
+          <ImageSend /> */}
+          <MessageSend onGeneratedMessage={handleGeneratedMessage} />
+          <ImageSend initialPrompt={generatedPrompt} />
         </div>
-    );
+      </div>
+      <div className="send-contact-section">
+        <SendPageContact />
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default SendPage;
