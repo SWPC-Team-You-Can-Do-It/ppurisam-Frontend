0b8c1a2a64c9ff261b489f88621510873fd56243
bkw535 <bkw54570@gmail.com>
2024-11-28 22:17:40 +0900

    feat: 폰트 추가

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App"; // App 컴포넌트 임포트
 import { AuthProvider } from "./components/login/AuthContext";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
